a46505a67cee9a569f9ed13e29ad33b942ae8e00
Roman Maher <roman.mager@outlook.com>
2025-10-25 23:12:58 +0200

    feat: implement create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
   // Write your code here
+  const fileName = "fresh.txt";
+  const content = "I am fresh and young";
+  const filesDirPath = path.join(__dirname, "files");
+  const filePath = path.join(filesDirPath, fileName);
+
+  try {
+    await fs.writeFile(filePath, content, { flag: "wx+" });
+  } catch (error) {
+    if (error.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+
+    throw error;
+  }
 };
 
 await create();
