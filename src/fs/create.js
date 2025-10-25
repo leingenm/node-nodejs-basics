f444007cad0a881b50712343b8ee5afe54d0b92e
Roman Maher <roman.mager@outlook.com>
2025-10-25 23:54:25 +0200

    refactor: update getting current dir path in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,12 @@
 import path from "node:path";
 import fs from "node:fs/promises";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const create = async () => {
   // Write your code here
   const fileName = "fresh.txt";
   const content = "I am fresh and young";
-  const filesDirPath = path.join(__dirname, "files");
+  const currentDirectory = import.meta.dirname;
+  const filesDirPath = path.join(currentDirectory, "files");
   const filePath = path.join(filesDirPath, fileName);
 
   try {
