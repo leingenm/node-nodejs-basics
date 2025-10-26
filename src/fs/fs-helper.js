237066c57a71fac36347ebf10481c2d278331708
Roman Maher <roman.mager@outlook.com>
2025-10-26 18:49:29 +0100

    refactor: add method to fs-helper to get files dir path

--- src/fs/fs-helper.js
+++ src/fs/fs-helper.js
@@ -2,8 +2,10 @@ import path from "node:path";
 
 export const FS_ERROR_MESSAGE = "FS operation failed";
 
+export const getFilesDirPath = () => path.join(import.meta.dirname, "files")
+
 export const getFilePathInFilesDir = (fileName) => {
-  const filesDir = path.join(import.meta.dirname, "files");
+  const filesDir = getFilesDirPath()
 
   return path.join(filesDir, fileName);
 };
