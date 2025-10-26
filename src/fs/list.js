cb3c041ff034d4e3b819e792974ffc0ea5640889
Roman Maher <roman.mager@outlook.com>
2025-10-26 12:12:50 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
 const list = async () => {
   // Write your code here
+  const filesDirPath = path.join(import.meta.dirname, "files");
+
+  try {
+    const dirContent = await fs.readdir(filesDirPath, {
+      recursive: true,
+    });
+    console.log(dirContent);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
