63685c7b999ee2bbf59b66bc07130ffca67c4261
Roman Maher <roman.mager@outlook.com>
2025-10-26 22:25:24 +0100

    feat: implement read.js in streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { pipeline } from "node:stream/promises";
+import { createReadStream } from "node:fs";
+import path from "node:path";
+
 const read = async () => {
-  // Write your code here
+  try {
+    const filePath = path.join(import.meta.dirname, "files", "fileToRead.txt");
+    const readStream = createReadStream(filePath, { encoding: "utf-8" });
+    await pipeline(readStream, process.stdout);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await read();
