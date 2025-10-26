dd390c2047819eabb39b2e15af7cfbb30c71c3c2
Roman Maher <roman.mager@outlook.com>
2025-10-26 22:44:13 +0100

    feat: implement write.js in streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { pipeline } from "node:stream/promises";
+import { createWriteStream } from "node:fs";
+import path from "node:path";
+
 const write = async () => {
-  // Write your code here
+  const filePath = path.join(import.meta.dirname, "files", "fileToWrite.txt");
+  const writeStream = createWriteStream(filePath, { encoding: "utf-8" });
+  await pipeline(process.stdin, writeStream);
 };
 
 await write();
