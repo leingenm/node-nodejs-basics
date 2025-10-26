26540a6e31d19a40720108da2066fe7f7ae26d11
Roman Maher <roman.mager@outlook.com>
2025-10-26 22:16:24 +0100

    fix: remove 1 from filename

--- src/fs/read.js
+++ src/fs/read.js
@@ -3,7 +3,7 @@ import { FS_ERROR_MESSAGE, getFilePathInFilesDir } from "./fs-helper.js";
 
 const read = async () => {
   try {
-    const filePath = getFilePathInFilesDir("fileToRead1.txt");
+    const filePath = getFilePathInFilesDir("fileToRead.txt");
     const content = await fs.readFile(filePath, 'utf-8');
     console.log(content);
   } catch {
