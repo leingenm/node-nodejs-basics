9bfd522e77b487628b8a89b88ba0654cdcf1ed96
Roman Maher <roman.mager@outlook.com>
2025-10-26 19:09:19 +0100

    feat: implement env.js function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-  // Write your code here
+  const prefix = "RSS_";
+  Object.entries(process.env)
+    .filter(([key]) => key.startsWith(prefix))
+    .forEach(([key, value]) => console.log(`${key}=${value};`));
 };
 
 parseEnv();
